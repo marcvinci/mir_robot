0b3de64cb82fcfc32b3647c9ee2b5e9830209e79
Marc Vinci <marc.vinci@dfki.de>
2025-09-09 14:59:18 +0200

    Changed the fake_mir_joint_publisher node to use a ros2 timer instead of blocking the node by sleeping at a fixed rate

--- mir_driver/mir_driver/fake_mir_joint_publisher.py
+++ mir_driver/mir_driver/fake_mir_joint_publisher.py
@@ -12,38 +12,44 @@ class fake_mir_joint_publisher(Node):
         super().__init__('fake_mir_joint_publisher')
 
         self.declare_parameter('tf_prefix', '')
-        tf_prefix = self.get_parameter('tf_prefix').get_parameter_value().string_value.strip('/')
-        if tf_prefix != "":
-            tf_prefix = tf_prefix + '/'
+        self.tf_prefix = self.get_parameter('tf_prefix').get_parameter_value().string_value.strip('/')
+        if self.tf_prefix != "":
+            self.tf_prefix = self.tf_prefix + '/'
 
-        pub = self.create_publisher(
+        self.pub = self.create_publisher(
                 msg_type=JointState,
                 topic='joint_states',  # no prefix to joint states, just namespace
                 qos_profile=qos_profile_system_default  # TODO Check QoS Settings
         )
 
-        pub_rate = 1
+        pub_rate = 1.0  # seconds
+        self.timer = self.create_timer(pub_rate, self.publish_joint_states)
 
-        while(rclpy.ok()):
-            js = JointState()
-            js.header.stamp = self.get_clock().now().to_msg()
-            js.name = [tf_prefix + 'left_wheel_joint', tf_prefix + 'right_wheel_joint',
-                       tf_prefix + 'fl_caster_rotation_joint', tf_prefix + 'fl_caster_wheel_joint',
-                       tf_prefix + 'fr_caster_rotation_joint', tf_prefix + 'fr_caster_wheel_joint',
-                       tf_prefix + 'bl_caster_rotation_joint', tf_prefix + 'bl_caster_wheel_joint',
-                       tf_prefix + 'br_caster_rotation_joint', tf_prefix + 'br_caster_wheel_joint']
-            js.position = [0.0 for _ in js.name]
-            js.velocity = [0.0 for _ in js.name]
-            js.effort = [0.0 for _ in js.name]
-            pub.publish(js)
-            time.sleep(pub_rate)
+    def publish_joint_states(self):
+        js = JointState()
+        js.header.stamp = self.get_clock().now().to_msg()
+        js.name = [self.tf_prefix + 'left_wheel_joint', self.tf_prefix + 'right_wheel_joint',
+                   self.tf_prefix + 'fl_caster_rotation_joint', self.tf_prefix + 'fl_caster_wheel_joint',
+                   self.tf_prefix + 'fr_caster_rotation_joint', self.tf_prefix + 'fr_caster_wheel_joint',
+                   self.tf_prefix + 'bl_caster_rotation_joint', self.tf_prefix + 'bl_caster_wheel_joint',
+                   self.tf_prefix + 'br_caster_rotation_joint', self.tf_prefix + 'br_caster_wheel_joint']
+        js.position = [0.0 for _ in js.name]
+        js.velocity = [0.0 for _ in js.name]
+        js.effort = [0.0 for _ in js.name]
+        self.pub.publish(js)
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = fake_mir_joint_publisher()
-    rclpy.spin(node)
-    rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 
 if __name__ == '__main__':
